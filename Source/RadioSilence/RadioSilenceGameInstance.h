f530fab3db0557af85021bb4faf9d34aedaeb691
helisoya <oudot.julien@laposte.net>
2024-02-07 16:26:06 +0100

    Open World I

---
+++ Source/RadioSilence/RadioSilenceGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RadioSilenceGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RADIOSILENCE_API URadioSilenceGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
